9acf221c59cd7fc34601c6151574f6d959b404a6
Kamau Jack <emarketingharleys@gmail.com>
2024-02-11 17:41:24 +0300

    new changes

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import styles from "./links.module.css";
+import NavLinks from "./navlinks/NavLinks";
+import { useState } from "react";
+const links = [
+  { title: "Home", path: "/" },
+  { title: "About", path: "/about" },
+  { title: "Contact", path: "/contact" },
+  { title: "Blog", path: "/blog" },
+];
+const Links = () => {
+  const [open, setOpen] = useState(false);
+  const session = true;
+  const isAdmin = true;
+  return (
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLinks item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLinks item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLinks item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <button onClick={() => setOpen((prev) => !prev)}>Menu</button>
+      {open && (
+        <div className={styles.mobilelinks}>
+          {links.map((link) => (
+            <NavLinks item={link} key={link.title} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Links;
