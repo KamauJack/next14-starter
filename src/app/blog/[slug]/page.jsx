3349538435373ef33da66bc6e5bf4e71f09d30a6
Kamau Jack <emarketingharleys@gmail.com>
2024-02-12 21:24:33 +0300

    12th Feb

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,45 @@
+import Image from "next/image";
+import styles from "./singlepost.module.css";
 const SinglePostPage = () => {
-  return <div>SinglePostPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/contact.png"
+          fill
+          alt="post title"
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            src="/contact.png"
+            width={50}
+            height={50}
+            alt="post title"
+            className={styles.avator}
+          />
+          <div className={styles.detailsText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Jax Maverick</span>
+          </div>
+          <div className={styles.detailsText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01 Nov</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempore,
+            delectus inventore qui accusamus animi laudantium minima tempora
+            beatae quibusdam.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
